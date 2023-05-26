32a20dde4515d4f205204b222525d820c9503e3a
Minakshi <Minakshi@DESKTOP-NVU3C2H>
2023-05-26 15:56:49 +0530

    restassuredtestng_allurereport

---
+++ src/CommonFunction/API_Common_Function.java
@@ -0,0 +1,32 @@
+package CommonFunction;
+
+//import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.given;
+
+
+import io.restassured.RestAssured;
+
+
+public class API_Common_Function{
+	
+	public static int res_statusCode(String baseURI,String requestbody,String resource) {
+	
+	//RestAssured.baseURI=baseURI;
+		RestAssured.baseURI=baseURI;
+	
+	int statusCode= given().header("Content-type","application/json").body(requestbody).when().post(resource).then().extract().response().statusCode();
+	
+	return statusCode;
+	}
+	
+	
+	
+	public static String response_body(String baseURI,String requestbody,String resource) {
+	
+	RestAssured.baseURI=baseURI;
+	
+	String responsebody=given().header("Content-type","application/json").body(requestbody).when().post(resource).then().extract().response().asString();
+	return responsebody;
+    }
+}
+
---
+++ src/testClass/Post_TC_2.java
@@ -0,0 +1,76 @@
+package testClass;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import CommonFunction.API_Common_Function;
+import CommonFunction.Utility_common_Function;
+import io.restassured.path.json.JsonPath;
+import requestRepository.Post_req_repository;
+
+    public class Post_TC_2 {
+    	@Test
+	  public static void execute() throws IOException {
+	 	for(int i=0; i<5; i++)
+		{
+		
+		 int statusCode=API_Common_Function.res_statusCode(Post_req_repository.base_URI(),
+    		                     Post_req_repository.Post_req_tc2(),Post_req_repository.post_resource());
+  
+           if(statusCode == 201)
+           {
+    	
+                String responsebody=API_Common_Function.response_body(Post_req_repository.base_URI(),
+        		                  Post_req_repository.Post_req_tc2(),Post_req_repository.post_resource());
+                Post_TC_2.validator(responsebody,statusCode);
+                Utility_common_Function.evidencefilecreator("Post_TC_2", Post_req_repository.Post_req_tc2(), responsebody);
+                break;
+        
+           }
+           else
+           {
+    	     System.out.println("correct status code is not found hence retrying the API");
+           }
+	
+	     }	
+	}
+			
+    public static void validator(String responsebody,int statusCode) throws IOException {
+		    	//Parse response body and its parameters
+				JsonPath jspres=new JsonPath(responsebody);
+				String res_name=jspres.getString("name");
+				String res_job=jspres.getString("job");
+				String res_id=jspres.getString("id");
+				String res_createdAt=jspres.getString("createdAt");
+				System.out.println(res_name);
+				System.out.println(res_job);
+				System.out.println(res_id);
+				System.out.println(res_createdAt);
+				
+				String trim_date=res_createdAt.substring(0,10);
+				
+				//generate date
+			   LocalDateTime date=LocalDateTime.now();
+			   String exp_date=date.toString().substring(0,10);
+			   
+			   
+			  //parse request body and its parameters
+			   JsonPath jspreq=new JsonPath (Post_req_repository.Post_req_tc2());
+				String req_name=jspreq.getString("name");
+				String req_job=jspreq.getString("job");
+			
+		    	//Validate the response
+			   
+			   Assert.assertEquals(statusCode,201);
+			   Assert.assertEquals(req_name, res_name);
+			   Assert.assertEquals(req_job, res_job);
+			   Assert.assertNotNull(res_id);
+			   Assert.assertEquals(trim_date, exp_date);
+			   
+			
+			}
+
+		}
